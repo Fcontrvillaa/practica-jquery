4148c42a579b98c4a8ad85619a39b4373f630055
Fcontrvillaa <fcontreras+1@gmail.com>
2025-04-24 03:04:46 +0000

    practica

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+
+
+$("p").click(function(){
+    $(this).hide(4000)
+    $(this).fadeIn(1000)
+})
+
+$("#boton").click(function(){
+    $("#div").css({
+        'with': '500px',
+        'backgroun-color': 'white';
+
+    })
+
+})
